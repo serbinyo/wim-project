1a783217e57e0539120d53e41f6495c07f0818ed
Сербин Александр <serbin@rarus.ru>
2022-03-06 16:14:17 +0300

    Добавлены методы для корректной работы с сущностями Комментарий и Конференция в админке

--- app/src/Controller/Admin/CommentCrudController.php
+++ app/src/Controller/Admin/CommentCrudController.php
@@ -4,6 +4,7 @@ namespace App\Controller\Admin;
 
 use App\Entity\Comment;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 
 /**
  *
@@ -15,14 +16,16 @@ class CommentCrudController extends AbstractCrudController
         return Comment::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
+        $fields = parent::configureFields($pageName);
+        $fields[] = AssociationField::new('conference');
+        return $fields;
+
+//        return [
+//            IdField::new('id'),
+//            TextField::new('title'),
+//            TextEditorField::new('description'),
+//        ];
     }
-    */
 }
