41203d390bf8ac28c8de45dbd93fe196ded2a7ee
Сербин Александр <serbin@rarus.ru>
2022-03-06 19:08:10 +0300

    Добавлена постраничная навигация для комментариев

--- app/src/Controller/ConferenceController.php
+++ app/src/Controller/ConferenceController.php
@@ -6,6 +6,7 @@ use App\Entity\Conference;
 use App\Repository\CommentRepository;
 use App\Repository\ConferenceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Twig\Environment;
@@ -53,19 +54,22 @@ class ConferenceController extends AbstractController
      * @throws LoaderError
      */
     public function show(
+        Request $request,
         Environment $twig,
         Conference $conference,
         CommentRepository $commentRepository
     ): Response {
+        $offset = max(0, $request->query->getInt('offset', 0));
+        $paginator = $commentRepository->getCommentPaginator($conference, $offset);
+
         return new Response(
             $twig->render(
                 'conference/show.html.twig',
                 [
                     'conference' => $conference,
-                    'comments'   => $commentRepository->findBy(
-                        ['conference' => $conference],
-                        ['createdAt' => 'DESC']
-                    ),
+                    'comments'   => $paginator,
+                    'previous' => $offset - CommentRepository::PAGINATOR_PER_PAGE,
+                    'next' => min(count($paginator), $offset + CommentRepository::PAGINATOR_PER_PAGE)
                 ]
             )
         );
--- app/src/Repository/CommentRepository.php
+++ app/src/Repository/CommentRepository.php
@@ -3,9 +3,11 @@
 namespace App\Repository;
 
 use App\Entity\Comment;
+use App\Entity\Conference;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\ORM\OptimisticLockException;
 use Doctrine\ORM\ORMException;
+use Doctrine\ORM\Tools\Pagination\Paginator;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
@@ -16,6 +18,11 @@ use Doctrine\Persistence\ManagerRegistry;
  */
 class CommentRepository extends ServiceEntityRepository
 {
+    /**
+     *
+     */
+    public const PAGINATOR_PER_PAGE = 2;
+
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Comment::class);
@@ -43,6 +50,20 @@ class CommentRepository extends ServiceEntityRepository
         if ($flush) {
             $this->_em->flush();
         }
+    }
+
+    public function getCommentPaginator(Conference $conference, int $offset): Paginator
+    {
+        $query = $this->createQueryBuilder('c')
+            ->andWhere('c.conference = :conference')
+            ->setParameter('conference', $conference)
+            ->orderBy('c.createdAt', 'DESC')
+            ->setMaxResults(self::PAGINATOR_PER_PAGE)
+            ->setFirstResult($offset)
+            ->getQuery()
+        ;
+
+        return new Paginator($query);
     }
 
     // /**
