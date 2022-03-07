0ec3d4da3aaf577a7bcecb28a144cfa5bffd1b2e
Сербин Александр <serbin@rarus.ru>
2022-03-07 14:50:29 +0300

    Добавлена форма обратной связи для добавления комментария. Добавляется командой php bin/console make:form Выведена в представлении

---
+++ app/src/Form/CommentFormType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Comment;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+/**
+ * Class CommentFormType
+ *
+ * @package App\Form
+ */
+class CommentFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('author')
+            ->add('text')
+            ->add('email')
+            ->add('photoFilename')
+            ->add('conference')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Comment::class,
+        ]);
+    }
+}
