7086a32594750a340efa447cf8e9ebbdbf4de61e
Сербин Александр <serbin@rarus.ru>
2022-03-12 12:38:10 +0300

    Установлен компонент messenger и настроена асинхронная обработка комментарий

--- app/src/Repository/CommentRepository.php
+++ app/src/Repository/CommentRepository.php
@@ -51,8 +51,10 @@ class CommentRepository extends ServiceEntityRepository
     public function getCommentPaginator(Conference $conference, int $offset): Paginator
     {
         $query = $this->createQueryBuilder('c')
-            ->andWhere('c.conference = :conference')
             ->setParameter('conference', $conference)
+            ->setParameter('state', 'published')
+            ->andWhere('c.conference = :conference')
+            ->andWhere('c.state = :state')
             ->orderBy('c.createdAt', 'DESC')
             ->setMaxResults(self::PAGINATOR_PER_PAGE)
             ->setFirstResult($offset)
