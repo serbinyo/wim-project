df03c3658db82ea5d5480dc689ea1f54b3f761f8
Сербин Александр <serbin@rarus.ru>
2022-03-19 18:08:07 +0300

    WIM-9 - Mailer. Добавлена отправка тестового email сообщения

---
+++ app/src/Service/Mailer.php
@@ -0,0 +1,68 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Service;
+
+use App\Entity\User;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Address;
+use Symfony\Component\Mime\Email;
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
+
+/**
+ * Class Mailer
+ *
+ * @package App\Service
+ */
+class Mailer
+{
+    /**
+     *
+     */
+    public const FROM_ADDRESS = 'serbinyo@yandex.ru';
+
+    /**
+     * @var MailerInterface
+     */
+    private $mailer;
+
+    /**
+     * @var Environment
+     */
+    private $twig;
+
+    public function __construct(
+        MailerInterface $mailer,
+        Environment $twig
+
+    )  {
+        $this->mailer = $mailer;
+        $this->twig = $twig;
+
+    }
+
+    /**
+     *
+     * @throws TransportExceptionInterface
+     */
+    public function sendTestEmail()
+    {
+        $email = (new Email())
+            //->from(self::FROM_ADDRESS)
+            ->from(new Address(self::FROM_ADDRESS, 'Алексис Сербин'))
+            ->to('serbinyo@gmail.com')
+            //->cc('cc@example.com')
+            //->bcc('bcc@example.com')
+            //->replyTo('fabien@example.com')
+            //->priority(Email::PRIORITY_HIGH)
+            ->subject('Тестовое сообщение разработки!')
+            ->html('<h2>Это тестовое сообщение разработки</h2>');
+
+        $this->mailer->send($email);
+    }
+}
