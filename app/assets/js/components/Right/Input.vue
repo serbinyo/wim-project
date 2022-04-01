a022e7a12f3d10cac189f9f3178cbccdffa4d661
Сербин Александр <serbin@rarus.ru>
2022-04-01 15:32:22 +0300

    Vue js. Добавлена блокировка кнопки при отправке сообщения

--- app/assets/js/components/Right/Input.vue
+++ app/assets/js/components/Right/Input.vue
@@ -2,10 +2,14 @@
     <!-- Typing area -->
     <form class="bg-light">
         <div class="input-group">
-            <input type="text" placeholder="Type a message" v-model="content" @keyup.prevent.enter="sendMessage"
+            <input type="text" placeholder="Напишите сообщение" id="chat-input"
+                   v-model="content"
+                   @keydown.enter.prevent
+                   @keyup.enter.prevent="sendMessage"
                    aria-describedby="button-addon2" class="form-control rounded-0 border-0 py-4 bg-light">
             <div class="input-group-append">
-                <button id="button-addon2" type="submit" class="btn btn-link"> <i @click.prevent="sendMessage" class="fa fa-paper-plane"></i></button>
+                <button id="button-addon2" type="submit"
+                        class="btn btn-link"> <i @click.prevent="sendMessage" class="fa fa-paper-plane"></i></button>
             </div>
         </div>
     </form>
@@ -20,12 +24,22 @@
             sendMessage() {
                 if ('' !== this.content) {
 
+                    //Блокируем инпут и кнопку
+                    const input = document.querySelector("#chat-input");
+                    input.disabled = true;
+                    const button = document.querySelector("#button-addon2");
+                    button.disabled = true;
+
                     this.$store.dispatch("POST_MESSAGE", {
                         conversationId: this.$route.params.id,
                         content: this.content
                     }).
                     then(() => {
-                        this.content = ''
+                        //Разблокируем инпут и кнопку
+                        this.content = '';
+                        input.disabled = false;
+                        button.disabled = false;
+                        input.focus();
                     })
                 }
             }
